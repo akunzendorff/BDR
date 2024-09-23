2208f44784bb0a8e6673400b9f79c5a9bb4c633a
Ana Flávia V. Kunzendorff <142523426+akunzendorff@users.noreply.github.com>
2024-09-23 11:37:48 -0300

    Update AbelhaBD.sql

--- BDR/BD_Abelha/AbelhaBD.sql
+++ BDR/BD_Abelha/AbelhaBD.sql
@@ -7,7 +7,6 @@ nome varchar(100),
 email varchar(100),
 login varchar(100),
 senha varchar(50),
-telefone varchar(15),
 acesso enum("Funcionário", "Gerente", "Supervisor") default ("Funcionário")
 );
 
@@ -15,7 +14,16 @@ create table colmeias(
 id int primary key auto_increment,
 nome varchar(100),
 localizacao varchar(255),
-tipo enum("Jataí", "Mirim", "Mandaguari", "Borá") default (null)
+tamanho varchar(50),
+tipo_abelha enum("Jataí", "Mirim", "Mandaguari", "Borá") default (null)
+);
+
+create table registroBackups (
+id int primary key auto_increment,
+data_backup datetime,
+tipo_backup enum('completo', 'incremental', 'diferencial'),
+caminho_arquivo varchar(255),
+sucesso boolean
 );
 
 create table analises(
@@ -24,46 +32,51 @@ id_colmeia int,
 id_usuario int,
 data_analise datetime,
 varroa boolean,
+imagens blob,
+temperatura decimal(5,2) not null,
+umidade decimal(5,2) not null,
 constraint id_colmeia_fk foreign key (id_colmeia) references colmeias(id),
-constraint id_usuario_fk foreign key (id_usuario) references usuarios(id)); 
+constraint id_usuario_fk foreign key (id_usuario) references usuarios(id)
+); 
 
-insert into usuarios(nome, email, login, senha, telefone, acesso) values
-("Ana Flávia", "ana@abelha.com", "ana", "ana123", "(13) 99999-9999", "Funcionário"),
-("Gustavo", "gustavo@abelha.com", "gustavo", "gustavo123", "(13) 99999-9990", "Gerente"),
-("Isabely", "isabely@abelha.com", "isabely", "isabely123", "(13) 99999-9991", "Supervisor"),
-("Yasmin", "yasmin@abelha.com", "yasmin", "yasmin123", "(13) 99999-9992", null);
+insert into usuarios(nome, email, login, senha, acesso) values
+("Ana Flávia", "ana@abelha.com", "ana", "ana123", "Funcionário"),
+("Gustavo", "gustavo@abelha.com", "gustavo", "gustavo123", "Gerente"),
+("Isabely", "isabely@abelha.com", "isabely", "isabely123", "Supervisor"),
+("Yasmin", "yasmin@abelha.com", "yasmin", "yasmin123", null);
 
-insert into colmeias (nome, localizacao, tipo) values
-("Colmeia 1", "41°24'12.2'N 2°10'26.5'E", "Jataí"),
-("Colmeia 2", "41°24'12.2'N 2°10'26.4'E", "Mirim"),
-("Colmeia 3", "41°24'12.2'N 2°10'26.3'E", "Borá"),
-("Colmeia 4", "41°24'12.2'N 2°10'26.2'E", "Mandaguari");
+insert into colmeias (nome, localizacao, tamanho, tipo_abelha) values
+("Colmeia 1", "41°24'12.2'N 2°10'26.5'E", "Grande", "Jataí"),
+("Colmeia 2", "41°24'12.2'N 2°10'26.4'E", "Média", "Mirim"),
+("Colmeia 3", "41°24'12.2'N 2°10'26.3'E", "Pequena", "Borá"),
+("Colmeia 4", "41°24'12.2'N 2°10'26.2'E", null, "Mandaguari");
+
+insert into registroBackups (data_backup, tipo_backup, caminho_arquivo, sucesso) values
+(now(), 'completo', '/caminho/do/backup.bak', true);
+
+insert into analises (id_colmeia, id_usuario, data_analise, varroa, temperatura, umidade) values
+(1, 4, "2024-09-19 10:00:00", "FALSE", 20.0, 20.0),
+(2, 3, "2024-09-19 11:00:00", "FALSE", 15.5, 15.5),
+(3, 2, "2024-09-19 12:00:00", "FALSE", 18.0, 18.8),
+(4, 1, "2024-09-19 13:00:00", "FALSE", 19.0, 17.5);
 
 
-insert into analises (id_colmeia, id_usuario, data_analise, varroa) values
-(1, 4, "2024-09-19 10:00:00", "FALSE"),
-(2, 3, "2024-09-19 11:00:00", "FALSE"),
-(3, 2, "2024-09-19 12:00:00", "FALSE"),
-(4, 1, "2024-09-19 13:00:00", "FALSE");
-
-
-SELECT 
-    u.id AS usuario_id,
-    u.nome AS usuario_nome,
-    u.email AS usuario_email,
-    u.login AS usuario_login,
-    u.telefone AS usuario_telefone,
-    u.acesso AS usuario_acesso,
-    c.id AS colmeia_id,
-    c.nome AS colmeia_nome,
-    c.localizacao AS colmeia_localizacao,
-    c.tipo AS colmeia_tipo,
-    a.id AS analise_id,
-    a.data_analise,
-    a.varroa
-FROM 
-    usuarios u
-LEFT JOIN 
-    analises a ON u.id = a.id_usuario
-LEFT JOIN 
-    colmeias c ON a.id_colmeia = c.id;
+select 
+    u.id as "ID do Usuário",
+    u.nome as "Nome do Usuário",
+    u.email as "Email do Usuário",
+    u.login as "Login do Usuário",
+    u.acesso as "Acesso do Usuário",
+    c.id as "ID da Colmeia",
+    c.nome as "Nome da Colmeia",
+    c.localizacao as "Localização da Colmeia",
+    c.tamanho as "Tamanho da Colmeia",
+    c.tipo_abelha as "Tipo de abelha",
+    a.id as "ID da Análise",
+    a.data_analise as "Data da Análise",
+    a.varroa as "Infestação",
+    a.temperatura as "Temperatura da Colmeia",
+    a.umidade as "Umidade da Colmeia"
+from  usuarios u
+left join analises a on u.id = a.id_usuario
+left join colmeias c on a.id_colmeia = c.id;
